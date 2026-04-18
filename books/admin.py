5d3d1f953a7d1a0e6f8137ddf207a43262479396
Jocelyn Sickler <3112960S@student.gla.ac.uk>
2026-04-18 21:15:06 +0100

    Remove duplicate files

--- books/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book, Cart, CartItem, Order, OrderItem
-
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Cart)
-admin.site.register(CartItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
